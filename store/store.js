de0d81aecb66f7a62d54d5ee588a9fbe54bbebf6
asdrubal <asdrubalgg@gmail.com>
2023-09-03 10:53:58 -0400

    Nuevo inicio de proyecto

--- store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { origenSlice } from "./miSlice"
-
-export default configureStore({
-    reducer:{
-        misValores: origenSlice.reducer
-    }
-})
